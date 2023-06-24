ae3cb6deb6b518ca4278018f30db13c4ea88a44a
Baptiste <63445829+Tegristh@users.noreply.github.com>
2023-06-24 16:37:42 +0200

    fin du tuto

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -33,6 +33,33 @@ def band_create(request):
                   'listings/band_create.html',
                   {'form': form})
 
+def band_update(request, id):
+    band = Band.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = BandForm(request.POST, instance=band)
+        if form.is_valid():
+            form.save()
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm(instance=band)
+
+    return render(request,
+                  'listings/band_update.html',
+                  {'form': form})
+
+def band_delete(request, id):
+    band = Band.objects.get(id=id)
+
+    if request.method == 'POST':
+        band.delete()
+        return redirect('band-list')
+    
+    return render(request, 
+                  'listings/band_delete.html',
+                  {'band': band})
+
+
 def about(request):
     return render(request, "listings/about.html")
 
@@ -59,6 +86,32 @@ def listing_create(request):
     return render(request, 
                   'listings/listing_create.html',
                   {'form': form})
+
+def listing_update(request, id):
+    listing = Listing.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            form.save()
+            return redirect('listing-detail', listing.id)
+    else:
+        form = ListingForm(instance=listing)
+
+    return render(request,
+                  'listings/band_update.html',
+                  {'form': form})
+
+def listing_delete(request, id):
+    listing = Listing.objects.get(id=id)
+
+    if request.method == 'POST':
+        listing.delete()
+        return redirect('listing-list')
+    
+    return render(request, 
+                  'listings/listing_delete.html',
+                  {'listing': listing})
 
 def contact(request):
     if request.method == 'POST':
